57e4e67d2d89451f8c100f3b42ad9c64c95793fe
Vitalii Tymashkov <vitalii.tymashkov.ctr@sabre.com>
2021-09-09 20:02:30 +0200

    Case 11 - refactoring - responsibilities breakdown
    (cherry picked from commit 25181058db2fc81e2fe335d98e109451b7c627fa)

--- Case11_SrpGodObject_Tymashkov/SpaceStation.java
+++ Case11_SrpGodObject_Tymashkov/SpaceStation.java
@@ -1,4 +1,4 @@
-package com.globallogic.unified.eight.cases.case11;
+package Case11_SrpGodObject_Tymashkov;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,63 +11,4 @@ public class SpaceStation {
         this.fuel = 0;
         this.supplies = new HashMap<>();
     }
-
-    public void runSensors(){
-        System.out.println("----- Sensor Action -----");
-        System.out.println("Running sensors!");
-    }
-
-    public void loadSupplies(String type, int quantity){
-        System.out.println("----- Supply Action -----");
-        System.out.printf("Loading %d units of %s in the supply hold.\n", quantity, type);
-
-        if (supplies.containsKey(type)){
-            supplies.put(type, supplies.get(type) + quantity);
-        } else {
-            supplies.put(type, quantity);
-        }
-    }
-
-    public void useSupplies(String type, int quantity){
-        System.out.println("----- Supply Action -----");
-        if(supplies.containsKey(type) && supplies.get(type)>quantity){
-            System.out.printf("Using %d of %s from the supply hold.\n", quantity, type);
-            supplies.put(type, supplies.get(type) - quantity);
-        } else {
-            System.out.printf("Supply Error: Insufficient %s in the supply hold.\n", type);
-        }
-    }
-
-    public void reportSupplies(){
-        System.out.println("----- Supply Report -----");
-        if(supplies.keySet().toArray().length!=0){
-            for (String type : supplies.keySet() ) {
-                System.out.printf("%s avalilable: %d unit(s)\n", type, supplies.get(type));
-            }
-        } else {
-            System.out.println("Supply hold is empty.");
-        }
-    }
-
-    public void loadFuel(int quantity){
-        System.out.println("----- Fuel Action -----");
-        System.out.printf("Loading %d units of fuel in the tank.\n", quantity);
-        this.fuel += quantity;
-    }
-
-    public void reportFuel(){
-        System.out.println("----- Fuel Report -----");
-        System.out.printf("%d units of fuel available.\n", fuel);
-    }
-
-    public void activateThrusters(){
-        System.out.println("----- Thruster Action -----");
-        if(fuel >= 10){
-            System.out.println("Thrusting action successful.");
-            this.fuel -= 10;
-        } else {
-            System.out.println("Thruster Error: Insufficient fuel available.");
-        }
-    }
-
 }
