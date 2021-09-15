6a2c6f205a21a128c689ab6fe6c830e30b890d71
Vitalii Tymashkov <vitalii.tymashkov.ctr@sabre.com>
2021-09-15 07:55:32 +0200

    @Override added to implementations

--- Case12_InterfacePollutionInAction_Tymashkov/BasicCalculator.java
+++ Case12_InterfacePollutionInAction_Tymashkov/BasicCalculator.java
@@ -3,19 +3,19 @@ package Case12_InterfacePollutionInAction_Tymashkov;
 import Case12_InterfacePollutionInAction_Tymashkov.calculatorInterfaces.Calculator;
 
 public class BasicCalculator implements Calculator {
-
+    @Override
     public int add(int num1, int num2) {
         return num1 + num2;
     }
-
+    @Override
     public int subtract(int num1, int num2) {
         return num1 - num2;
     }
-
+    @Override
     public int multiply(int num1, int num2) {
         return num1 * num2;
     }
-
+    @Override
     public double divide(int num1, int num2) {
         return (double) num1 / (double) num2;
     }
